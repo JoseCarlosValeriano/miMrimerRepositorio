815c6b4d3da20b6a7cfe19e707fb59248ec90eda
José Carlos Valeriano Rodríguez <carlos.v.r@gmx.es>
2021-01-28 14:09:00 +0000

    Update primer.py

--- primer.py
+++ primer.py
@@ -1,2 +1,9 @@
 # Primer fichero
 print("Hola")
+
+letra = input("dame una letra:")
+
+print("lo que escribiste fue: ", letra)
+
+numero = float(input " dame un número "))
+print("Dame un número", numero )
